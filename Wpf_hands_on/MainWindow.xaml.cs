705dad8a33dbaab2bd5a0da16af71d7fe6071210
Harithvarakesan Sakthivel <Harithvarakesan.bs@solitontech.com>
2025-12-10 14:49:27 +0530

    feat: Basic controls and Panels Hands-on

--- Wpf_hands_on/MainWindow.xaml.cs
+++ Wpf_hands_on/MainWindow.xaml.cs
@@ -20,5 +20,37 @@ namespace Wpf_hands_on
         {
             InitializeComponent();
         }
+
+        private void TextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            TextBox? textBox = sender as TextBox;
+            txtSelection.Text = "Selected text is '";
+            txtSelection.Text += textBox.SelectedText + "' with length of ";
+            txtSelection.Text += textBox.SelectionLength;
+        }
+
+        private void submitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            submitBtn.IsEnabled = false;
+            submitBtn.Content = "Submitted";
+        }
+
+        private void cbAllStack_Checked(object sender, RoutedEventArgs e)
+        {
+            bool cbAllStackStatus = (cbAllStack.IsChecked == true);
+            cStack.IsChecked = cbAllStackStatus;
+            pythonStack.IsChecked = cbAllStackStatus;
+            labviewStack.IsChecked = cbAllStackStatus;
+        }
+
+        private void cbStack_Changed(object sender, RoutedEventArgs e)
+        {
+            if (cStack.IsChecked == true && pythonStack.IsChecked == true && labviewStack.IsChecked == true)
+                cbAllStack.IsChecked = true;
+            else if (cStack.IsChecked == false && pythonStack.IsChecked == false && labviewStack.IsChecked == false)
+                cbAllStack.IsChecked = false;
+            else
+                cbAllStack.IsChecked = null;
+        }
     }
 }
